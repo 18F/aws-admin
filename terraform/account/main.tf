86c0648889f2e3dd34937398ffc0de00bd657b9d
John Jediny <JJediny@users.noreply.github.com>
2020-12-17 14:49:03 -0500

    moving alerting and logging modules org wide

--- terraform/account/main.tf
+++ terraform/account/main.tf
@@ -9,3 +9,16 @@ resource "aws_organizations_account" "account" {
     prevent_destroy = true
   }
 }
+
+module "logging" {
+  source                     = "github.com/GSA/grace-logging?ref=v0.0.11"
+  access_logging_bucket_name = "gsa-tts-grace-config-access"
+  cloudtrail_name            = "gsa-tts-wide"
+  logging_bucket_name        = "gsa-tts-grace-config-logging"
+}
+
+module "alerting" {
+    source                    = "github.com/GSA/grace-alerting?ref=v0.0.3"
+    cloudtrail_log_group_name = "gsa-tts-wide"
+    recipient                 = "18fsoftware@gsa.gov"
+}
