fefc6dec1d86e8e9c1ef0659124a1b0ef3e4a32f
John Jediny <JJediny@users.noreply.github.com>
2020-12-17 15:09:41 -0500

    rolling out globally
    setting loose thresholds to avoid disturbing current account usage. adding back empty config dir to properly destroy first

--- terraform/config/main.tf
+++ terraform/config/main.tf
@@ -1,10 +1,3 @@
 provider "aws" {
   region  = "us-east-1"
 }
-module "config" {
-    source = "github.com/GSA/grace-config?ref=v0.0.3"
-    bucket = "gsa-tts-grace-config-logging"
-    config_snapshot_frequency = "Six_Hours"
-    iam_inactive_credentials_days = "120"
-    access_key_expiration_days = "3"
-}
